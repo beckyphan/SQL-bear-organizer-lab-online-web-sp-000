ecd3c3a4cd9f9532fcdd0fc22ebe8478edf6c220
Becky Phan <beckyphan@live.com>
2019-07-10 01:59:28 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -22,7 +22,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT MAX(color), COUNT() FROM bears GROUP BY color;"
+  "SELECT color, COUNT() FROM bears GROUP BY color LIMIT 1;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
